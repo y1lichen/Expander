6c9359029b676ddaeb0810b12057faa6097564db
JeffersonChen <f1jefferson2@gmail.com>
2021-08-08 14:07:18 +0800

    Initial Commit

---
+++ Expander/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Expander
+//
+//  Created by 陳奕利 on 2021/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
